dfb3ff8a0fa1855ec37f0649f6460c7a53fc5da3
Leslit Molina <113367291+lmolina613@users.noreply.github.com>
2022-10-27 20:12:05 -0400

    styled

--- enjoy-the-outdoors/scripts/mountains.js
+++ enjoy-the-outdoors/scripts/mountains.js
@@ -1,6 +1,6 @@
 function mountainImage(item){
     const img = document.createElement("img");
-    img.src = "enjoy-the-outdoors/images/" + item.img;
+    img.src = "/enjoy-the-outdoors/images/" + item.img;
     return img;
 }
 
@@ -11,15 +11,35 @@ function mountainTitle(item){
     return text;
 }
 
+function mountainLevel(item) {
+    const level = document.createElement("p");
+    level.classList.add("card-mtnLevel");
+    level.innerHTML = "<b>Effort Level: </b><br>";
+    level.innerHTML += item.effort;
+    return level;
+}
+
+function mountainInfo(item) {
+    const info = document.createElement("p");
+    info.classList.add("card-description");
+    info.innerHTML= "<b>Description: </b><br>";
+    info.innerHTML += item.desc;
+    return info;
+}
+
+
+
 function mountainCard(item) {
     const card = document.createElement("div");
-    card.innerText = item.desc
     card.classList.add("card");
-    card.appendChild(mountainImage(item));    
+    card.appendChild(mountainImage(item));
     card.appendChild(mountainTitle(item));
+    card.appendChild(mountainLevel(item));
+    card.appendChild(mountainInfo(item));
+
     return card;
 }
 
-document.addEventListener("DOMContentLoaded",()=>{
-    mountainsArray.forEach( m => document.body.appendChild(mountainCard(m)));
+document.addEventListener("DOMContentLoaded", () => {
+    mountainsArray.forEach(m => document.body.appendChild(mountainCard(m)));
 });
